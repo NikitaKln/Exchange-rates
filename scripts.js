6ca8de852fd01c88b702cc98d22f37801d0120d0
NikitaKln <klenikovnikl@gmail.com>
2023-12-04 22:34:16 +0300

    Рабочая версия с двумя валютами

--- scripts.js
+++ scripts.js
@@ -5,12 +5,11 @@ async function f() {
 let url = 'https://www.cbr-xml-daily.ru/daily_json.js';
 let response = await fetch(url);
 let json = await response.json(); // загружаем JSON с курсами с сервера
-let rateobject = JSON.parse(json);
 
 let value = document.getElementById("value");
 let result = document.getElementById("result");
-let rateleft = 1/rateobject.Valute.USD.Value;
-let rateright = rateobject.Valute.USD.Value;
+let rateleft = 1/json.Valute.USD.Value; //дефолтные значения
+let rateright = json.Valute.USD.Value;
 
 let rateplaque_left = document.getElementById("rateplaque_left");
 let rateplaque_right = document.getElementById("rateplaque_right");
@@ -18,11 +17,11 @@ let rateplaque_right = document.getElementById("rateplaque_right");
 rateplaque_left.innerText = rateleft;
 rateplaque_right.innerText = rateright;
 
-value.oninput = function () {
+value.oninput = function () {                   //калькулятор валют
   if (value.value == "") {
     result.value = "";
   } else {
-    let res = value.value * rateru;
+    let res = value.value * rateleft;
     result.value = res.toFixed(2);
   }
 };
@@ -31,7 +30,7 @@ result.oninput = function () {
   if (result.value == "") {
     value.value = "";
   } else {
-    let res = result.value * rateusd;
+    let res = result.value * rateright;
     value.value = res.toFixed(2);
   }
 };
