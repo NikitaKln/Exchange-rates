ad85c704e71e7e24d4f98c4850cc6a71f54369db
NikitaKln <klenikovnikl@gmail.com>
2024-05-20 19:31:24 +0300

    Добавлен свап подсветки

--- scripts.js
+++ scripts.js
@@ -11,22 +11,26 @@ async function f() {
   let valuteR = json.Valute.USD.Value; //дефолтные значения
   let valuteLChar = "RUR";
   let valuteRChar = "USD";
-  let rateleft = valuteL / valuteR;
-  let rateright = valuteR / valuteL;
+  let rateLeft = valuteL / valuteR;
+  let rateRight = valuteR / valuteL;
 
-  let rateplaque_left = document.getElementById("rateplaque_left");
-  let rateplaque_right = document.getElementById("rateplaque_right");
+  let rateplaqueLeft = document.getElementById("rateplaque_left");
+  let rateplaqueRight = document.getElementById("rateplaque_right");
 
-  rateplaque_left.innerText = `1 ${valuteLChar} = ${rateleft.toFixed(4)} ${valuteRChar}`;
-  rateplaque_right.innerText = `1 ${valuteRChar} = ${rateright.toFixed(4)} ${valuteLChar}`;
+  rateplaqueLeft.innerText = `1 ${valuteLChar} = ${rateLeft.toFixed(
+    4
+  )} ${valuteRChar}`;
+  rateplaqueRight.innerText = `1 ${valuteRChar} = ${rateRight.toFixed(
+    4
+  )} ${valuteLChar}`;
 
-    //калькулятор валют
+  //калькулятор валют
 
   value.oninput = function () {
     if (value.value == "") {
       result.value = "";
     } else {
-      let res = value.value * rateleft;
+      let res = value.value * rateLeft;
       result.value = res.toFixed(2);
     }
   };
@@ -35,7 +39,7 @@ async function f() {
     if (result.value == "") {
       value.value = "";
     } else {
-      let res = result.value * rateright;
+      let res = result.value * rateRight;
       value.value = res.toFixed(2);
     }
   };
@@ -52,38 +56,41 @@ async function f() {
   let gbpr = document.getElementById("GBPR");
 
   function update() {
-    rateleft = valuteL / valuteR;
-    rateright = valuteR / valuteL;
-    rateplaque_left.innerText = `1 ${valuteLChar} = ${rateleft.toFixed(4)} ${valuteRChar}`;
-    rateplaque_right.innerText = `1 ${valuteRChar} = ${rateright.toFixed(4)} ${valuteLChar}`;
-    let resleft = value.value * rateleft;
+    rateLeft = valuteL / valuteR;
+    rateRight = valuteR / valuteL;
+    rateplaqueLeft.innerText = `1 ${valuteLChar} = ${rateLeft.toFixed(
+      4
+    )} ${valuteRChar}`;
+    rateplaqueRight.innerText = `1 ${valuteRChar} = ${rateRight.toFixed(
+      4
+    )} ${valuteLChar}`;
+    let resleft = value.value * rateLeft;
     result.value = resleft.toFixed(2);
-    let resright = result.value * rateright;
+    let resright = result.value * rateRight;
     value.value = resright.toFixed(2);
   }
 
   function leftCurrencySelector(currency) {
-    if(currency == 1) {
+    if (currency == 1) {
       valuteL = 1;
-      valuteLChar = 'RUR';
+      valuteLChar = "RUR";
     } else {
-    valuteL = currency.Value;
-    valuteLChar = currency.CharCode;
+      valuteL = currency.Value;
+      valuteLChar = currency.CharCode;
     }
     update();
   }
 
   function rightCurrencySelector(currency) {
-    if(currency == 1) {
-      valuteR =1;
-      valuteRChar = 'RUR';
+    if (currency == 1) {
+      valuteR = 1;
+      valuteRChar = "RUR";
     } else {
-    valuteR = currency.Value;
-    valuteRChar = currency.CharCode;
+      valuteR = currency.Value;
+      valuteRChar = currency.CharCode;
     }
     update();
   }
-
 
   rurl.onclick = function () {
     leftCurrencySelector(1);
@@ -121,10 +128,13 @@ async function f() {
 
   let currencyArray = []; //массив валют для сортировки
 
-  Object.values(json.Valute).forEach((obj) => {     //сортируем в алфавитном порядке
-    currencyArray.push(obj);
+  Object.values(json.Valute).forEach((obj) => {
+    if (!["RUR", "USD", "EUR", "GBP"].includes(obj.CharCode)) {
+      currencyArray.push(obj);
+    }
   });
 
+  //сортируем в алфавитном порядке
   currencyArray.sort(function (a, b) {
     let firstLetterComparison = a.CharCode[0].localeCompare(b.CharCode[0]);
 
@@ -142,7 +152,7 @@ async function f() {
     button.className = "currency-select dropdown-button";
     button.type = "button";
     button.name = currencyArray[i].CharCode;
-    button.id = `${currencyArray[i].CharCode}L`
+    button.id = `${currencyArray[i].CharCode}L`;
     button.value = currencyArray[i].CharCode;
     button.onclick = function () {
       leftCurrencySelector(currencyArray[i]);
@@ -159,7 +169,7 @@ async function f() {
     button.className = "currency-select dropdown-button";
     button.type = "button";
     button.name = currencyArray[i].CharCode;
-    button.id = `${currencyArray[i].CharCode}R`
+    button.id = `${currencyArray[i].CharCode}R`;
     button.value = currencyArray[i].CharCode;
     button.onclick = function () {
       rightCurrencySelector(currencyArray[i]);
@@ -169,71 +179,135 @@ async function f() {
     othersRight.append(button);
   }
 
-  //кнопка SWAP (сделать свап подсветки)
+  //кнопка SWAP (свап подсветки работает только на 4 основных кнопках, сделать так чтобы он работал с выпадашкой)
 
   let swap = document.getElementById("swap");
 
   swap.onclick = function () {
-  let container = valuteL;
-  valuteL = valuteR;
-  valuteR = container;
-  container = valuteLChar;
-  valuteLChar = valuteRChar;
-  valuteRChar = container;
-  rateleft = valuteL/valuteR;
-  rateright = valuteR/valuteL;
-  rateplaque_left.innerText = `1 ${valuteLChar} = ${rateleft.toFixed(4)} ${valuteRChar}`;
-  rateplaque_right.innerText = `1 ${valuteRChar} = ${rateright.toFixed(4)} ${valuteLChar}`;
-  container = value.value;
-  value.value = result.value;
-  result.value = container;
+    let container = valuteL;
+    valuteL = valuteR;
+    valuteR = container;
+    container = valuteLChar;
+    valuteLChar = valuteRChar;
+    valuteRChar = container;
+    rateLeft = valuteL / valuteR;
+    rateRight = valuteR / valuteL;
+    rateplaqueLeft.innerText = `1 ${valuteLChar} = ${rateLeft.toFixed(
+      4
+    )} ${valuteRChar}`;
+    rateplaqueRight.innerText = `1 ${valuteRChar} = ${rateRight.toFixed(
+      4
+    )} ${valuteLChar}`;
+    container = value.value;
+    value.value = result.value;
+    result.value = container;
+
+    let temp;
+    let arrowTemp = arrowl;
+    let arrowLTempValue = arrowl.value;
+    let arrowRTempValue = arrowr.value;
+    let swapFlagRight = 0;
+    let swapFlagLeft = 0;
+
+    if (arrowl.classList.contains("highlight")) {
+      arrowl.classList.remove("highlight");
+      if (arrowr.classList.contains("highlight")) {
+        arrowl.value = arrowr.value;
+        arrowl.classList.add("highlight");
+      } else {
+        arrowl.value = "Другие";
+        swapFlagRight = 1;
+        arrowr.value = arrowLTempValue;
+      }
+    }
+
+    if (arrowr.classList.contains("highlight")) {
+      arrowr.classList.remove("highlight");
+      if (arrowTemp.classList.contains("highlight")) {
+        console.log(arrowTemp.value);
+        arrowr.value = arrowLTempValue;
+        arrowr.classList.add("highlight");
+      } else {
+        arrowr.value = "Другие";
+        swapFlagLeft = 1;
+        arrowl.value = arrowRTempValue;
+      }
+    }
+    if (swapFlagRight === 1) {
+      arrowr.classList.add("highlight");
+      swapFlagRight = 0;
+    }
+
+    if (swapFlagLeft === 1) {
+      arrowl.classList.add("highlight");
+      swapFlagLeft = 0;
+    }
+
+    if (leftSelectedButton) {
+      leftSelectedButton.classList.remove("highlight");
+      temp = leftSelectedButton;
+
+      leftSelectedButton = document.getElementById(
+        `${rightSelectedButton.value}L`
+      );
+
+      leftSelectedButton.classList.add("highlight");
+    }
+
+    if (rightSelectedButton) {
+      rightSelectedButton.classList.remove("highlight");
+      rightSelectedButton = document.getElementById(`${temp.value}R`);
+      rightSelectedButton.classList.add("highlight");
+    }
   };
 
   //подсветка кнопок
 
   let leftSelectedButton = rurl;
   let rightSelectedButton = usdr;
-  let leftUl = document.getElementById('leftul');
-  let rightUl = document.getElementById('rightul');
+  let leftUl = document.getElementById("leftul");
+  let rightUl = document.getElementById("rightul");
 
-  leftSelectedButton.classList.add('highlight');
-  rightSelectedButton.classList.add('highlight');
+  leftSelectedButton.classList.add("highlight");
+  rightSelectedButton.classList.add("highlight");
 
-  leftUl.onclick = function(event) {
-      let target = event.target.closest('input');
-      if (!target) return;
-      highlightLeft(target);
-  }
+  leftUl.onclick = function (event) {
+    let target = event.target.closest("input");
+    if (!target) return;
+    highlightLeft(target);
+  };
 
-  rightUl.onclick = function(event) {
-    let target = event.target.closest('input');
+  rightUl.onclick = function (event) {
+    let target = event.target.closest("input");
     if (!target) return;
     highlightRight(target);
-}
+  };
 
   function highlightLeft(elem) {
-    if(othersLeft.contains(elem)) {
-      arrowl.classList.add('highlight');
-    } else {arrowl.classList.remove('highlight');}
-    if(leftSelectedButton) {
-      leftSelectedButton.classList.remove('highlight');
+    if (othersLeft.contains(elem)) {
+      arrowl.classList.add("highlight");
+    } else {
+      arrowl.classList.remove("highlight");
+    }
+    if (leftSelectedButton) {
+      leftSelectedButton.classList.remove("highlight");
     }
     leftSelectedButton = elem;
-    leftSelectedButton.classList.add('highlight');
+    leftSelectedButton.classList.add("highlight");
   }
 
   function highlightRight(elem) {
-    if(othersRight.contains(elem)) {
-      arrowr.classList.add('highlight');
-    } else {arrowr.classList.remove('highlight')}
-    if(rightSelectedButton) {
-      rightSelectedButton.classList.remove('highlight');
+    if (othersRight.contains(elem)) {
+      arrowr.classList.add("highlight");
+    } else {
+      arrowr.classList.remove("highlight");
+    }
+    if (rightSelectedButton) {
+      rightSelectedButton.classList.remove("highlight");
     }
     rightSelectedButton = elem;
-    rightSelectedButton.classList.add('highlight');
+    rightSelectedButton.classList.add("highlight");
   }
-
-
 }
 
 window.onload = f();
--- table.js
+++ table.js
@@ -1,95 +1,102 @@
 "use strict";
 
 async function f() {
-    let url = 'https://www.cbr-xml-daily.ru/daily_json.js';
-    let response = await fetch(url);
-    let json = await response.json();
+  let url = "https://www.cbr-xml-daily.ru/daily_json.js";
+  let response = await fetch(url);
+  let json = await response.json();
+  const curInRow = 11;
 
-    let currencyArray = [];    //массив валют для сортировки 
-    
-    Object.values(json.Valute).forEach(obj => {         
-        currencyArray.push(obj);
-    }); 
+  let currencyArray = []; //массив валют для сортировки
 
-    currencyArray.push({
-        CharCode: 'RUR',
-        Value: 1
-    })
+  Object.values(json.Valute).forEach((obj) => {
+    currencyArray.push(obj);
+  });
 
-    //сортируем в алфавитном порядке
+  currencyArray.push({
+    CharCode: "RUR",
+    Value: 1,
+  });
 
-    currencyArray.sort(function(a, b) {
-        let firstLetterComparison = a.CharCode[0].localeCompare(b.CharCode[0]);
+  //сортируем в алфавитном порядке
 
-        if (firstLetterComparison === 0) {
-            return a.CharCode[1].localeCompare(b.CharCode[1]);
-        }
+  currencyArray.sort(function (a, b) {
+    let firstLetterComparison = a.CharCode[0].localeCompare(b.CharCode[0]);
 
-        return firstLetterComparison;
-    });
-
-    
-    //генерим содержимое таблицы
-
-    let table = document.getElementById('table');
-
-    for (let i=0; i<5; i++) {
-        let headerrow = document.createElement('tr');
-        table.append(headerrow);
-        for (let j=i*11; (j<((i*11) + 11))&&(j<currencyArray.length) ; j++) {
-            let th = document.createElement('th');
-            th.innerText = currencyArray[j].CharCode;
-            headerrow.append(th);
-        }
-        let datarow = document.createElement('tr');
-        table.append(datarow);
-        for (let  j=i*11; (j<((i*11) + 11))&&(j<currencyArray.length) ; j++) {
-            let td = document.createElement('td');
-            td.innerText = currencyArray[j].Value.toFixed(4);
-            td.id = currencyArray[j].CharCode;
-            datarow.append(td);
-        }
+    if (firstLetterComparison === 0) {
+      return a.CharCode[1].localeCompare(b.CharCode[1]);
     }
 
-    //генерим кнопки 
+    return firstLetterComparison;
+  });
 
-    let baseSelector = document.getElementById('table-dropdown-content');
-    let tableDropdown = document.getElementById('table-dropdown');
+  //генерим содержимое таблицы
 
-    for (let i=0; i<currencyArray.length; i++) {
-        let button = document.createElement('button');
-        button.innerText = currencyArray[i].CharCode;
-        button.className = 'table-dropdown-currency';
-        button.onclick = function () {
-            for (let j=0; j<currencyArray.length; j++) {
-            let td = document.getElementById(currencyArray[j].CharCode);
-            let result = currencyArray[j].Value/currencyArray[i].Value;
-            td.innerText = result.toFixed(4);
-            }
-            tableDropdown.innerText = currencyArray[i].CharCode;
-        }
-        baseSelector.append(button);
+  let table = document.getElementById("table");
+
+  for (let i = 0; i < 5; i++) {
+    let headerrow = document.createElement("tr");
+    table.append(headerrow);
+    for (
+      let j = i * curInRow;
+      j < i * curInRow + curInRow && j < currencyArray.length;
+      j++
+    ) {
+      let th = document.createElement("th");
+      th.innerText = currencyArray[j].CharCode;
+      headerrow.append(th);
     }
-
-    //подсветка кнопок
-
-    let dropdown = document.getElementById('table-dropdown-content');
-    let selectedButton;
-
-    dropdown.onclick = function(event) {
-        let target = event.target.closest('button');
-        if (!target) return;
-        highlight(target);
+    let datarow = document.createElement("tr");
+    table.append(datarow);
+    for (
+      let j = i * curInRow;
+      j < i * curInRow + curInRow && j < currencyArray.length;
+      j++
+    ) {
+      let td = document.createElement("td");
+      td.innerText = currencyArray[j].Value.toFixed(4);
+      td.id = currencyArray[j].CharCode;
+      datarow.append(td);
     }
+  }
 
-    function highlight(elem) {
-        if(selectedButton) {
-          selectedButton.classList.remove('table-highlight');
-        }
-        selectedButton = elem;
-        selectedButton.classList.add('table-highlight');
+  //генерим кнопки
+
+  let baseSelector = document.getElementById("table-dropdown-content");
+  let tableDropdown = document.getElementById("table-dropdown");
+
+  for (let i = 0; i < currencyArray.length; i++) {
+    let button = document.createElement("button");
+    button.innerText = currencyArray[i].CharCode;
+    button.className = "table-dropdown-currency";
+    button.onclick = function () {
+      for (let j = 0; j < currencyArray.length; j++) {
+        let td = document.getElementById(currencyArray[j].CharCode);
+        let result = currencyArray[j].Value / currencyArray[i].Value;
+        td.innerText = result.toFixed(4);
       }
-}
+      tableDropdown.innerText = currencyArray[i].CharCode;
+    };
+    baseSelector.append(button);
+  }
 
+  //подсветка кнопок
+
+  let dropdown = document.getElementById("table-dropdown-content");
+  let selectedButton;
+
+  dropdown.onclick = function (event) {
+    let target = event.target.closest("button");
+    if (!target) return;
+    highlight(target);
+  };
+
+  function highlight(elem) {
+    if (selectedButton) {
+      selectedButton.classList.remove("table-highlight");
+    }
+    selectedButton = elem;
+    selectedButton.classList.add("table-highlight");
+  }
+}
 
 window.onload = f();
